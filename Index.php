8a9bbda563d8c6bacc574cf6115ff6dfcefcc5db
PlayingWithCows <wiealbern@gmail.com>
2017-08-31 20:40:51 +0200

    first forms

--- Index.php
+++ Index.php
@@ -8,7 +8,26 @@
 
 
 <body>
+    <p>Log In:</p>
+    <form>
+        <input name="usernameLogin" type="text" placeholder="Username"/>
+        <input name="passwordLogin" type="password" placeholder="Password"/>
+        <button type="submit">Log In</button>
+    </form>
 
+    <br />
+    <br />
+    <div>
+        <p>Create Account:</p>
+        <form>
+            <input name="username" type="text" placeholder="Username"/>
+            <input name="password1" type="password" placeholder="Password"/>
+            <input name="password2" type="password" placeholder="Password" />
+            <input name="emailadd" type="email" placeholder="E-Mail Address" />
+            <button type="submit">Create Account</button>
+        </form>
+
+    </div>
 
 </body>
 </html>
