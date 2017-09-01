c38142f12fc0e6423de91414084254f64e1492d3
PlayingWithCows <wiealbern@gmail.com>
2017-09-01 21:07:32 +0200

    ready to test with data from POST form to create account

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,8 +1,12 @@
 <?php
 
-include_once 'includes/dbh.inc.php';
+include_once 'dbh.inc.php';
 
-$sql ="INSERT INTO users (user_email, user_name, user_pwd) VALUES ('playwithcows@gmail.com', 'PlayingWithCows', 'testpassword');";
+$email = $_POST['emailadd'];
+$username = $_POST['username'];
+$password = $_POST['password1'];
+
+$sql ="INSERT INTO users (user_email, user_name, user_pwd) VALUES ($email, $username, $password);";
 mysqli_query($conn, $sql);
 
 header("Location: ../index.php?signup=success");
