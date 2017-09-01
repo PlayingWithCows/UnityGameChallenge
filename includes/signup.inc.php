95ee4aa1af0d4b87d913571e998d89a6cb6d4a9f
PlayingWithCows <wiealbern@gmail.com>
2017-09-01 21:12:24 +0200

    fixed sql query

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -6,7 +6,7 @@ $email = $_POST['emailadd'];
 $username = $_POST['username'];
 $password = $_POST['password1'];
 
-$sql ="INSERT INTO users (user_email, user_name, user_pwd) VALUES ($email, $username, $password);";
+$sql ="INSERT INTO users (user_email, user_name, user_pwd) VALUES ('$email', '$username', '$password');";
 mysqli_query($conn, $sql);
 
 header("Location: ../index.php?signup=success");
