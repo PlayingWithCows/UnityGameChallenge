52ba086ade01ff07382f1a292f00599ae52cc069
PlayingWithCows <wiealbern@gmail.com>
2017-09-01 20:46:48 +0200

    created new users database for login system

--- Index.php
+++ Index.php
@@ -1,3 +1,9 @@
+<?php
+
+include_once 'includes/dbh.inc.php';
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -26,6 +32,28 @@
             <input name="emailadd" type="email" placeholder="E-Mail Address" />
             <button type="submit">Create Account</button>
         </form>
+
+    </div>
+    <br />
+    <br />
+    <div>
+        <?php
+
+        $sql ="SELECT * FROM users;";
+        $result = mysqli_query($conn, $sql);
+
+        $resultCheck = mysqli_num_rows($result);
+
+        if($resultCheck>0){
+            while ($row = mysqli_fetch_assoc($result))
+            {
+            	echo $row['user_name'];
+            }
+            
+        }
+
+
+        ?>
 
     </div>
 
--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,8 +1,8 @@
 <?php
 
 $dbServername = "shareddb1d.hosting.stackcp.net";
-$dbUsername = "Accounts-32314a22";
-$dbPassword ="testpassword!";
-$dbName = "loginsystem";
+$dbUsername = "loginsystem-32310e30";
+$dbPassword ="UuC3vN+H6sOm";
+$dbName = "loginsystem-32310e30";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName); 
