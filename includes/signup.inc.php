55b91219ea6a07567d21384b3ac2ff781ae5bbba
PlayingWithCows <wiealbern@gmail.com>
2017-09-02 16:50:51 +0200

    check if password is same test

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -5,7 +5,13 @@ include_once 'dbh.inc.php';
 $email = mysqli_real_escape_string($conn, $_POST['emailadd']);
 $username = mysqli_real_escape_string($conn, $_POST['username']);
 $password = mysqli_real_escape_string($conn, $_POST['password1']);
+$errorfound = false;
 
+if(!$password===mysqli_real_escape_string($conn, $_POST['password2']);){
+    $errorfound=true;
+}
+
+if(!$errorfound){
 $sql ="INSERT INTO users (user_email, user_name, user_pwd) VALUES (?, ?, ?);";
 $stmt = mysqli_stmt_init($conn);
 if(!mysqli_stmt_prepare($stmt,$sql)){
@@ -16,3 +22,5 @@ if(!mysqli_stmt_prepare($stmt,$sql)){
 }
 
 header("Location: ../index.php?signup=success");
+
+}else{"Location: ../index.php?signup=failed"}
